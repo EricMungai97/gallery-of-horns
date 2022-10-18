6ee2f5f1acc743265e379acb05b612e7658f22a6
EricMungai97 <mungaikinus@gmail.com>
2022-10-17 17:11:47 -0700

    added new js files

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <footer>&copy; Eric Kinuthia</footer>
+    )
+  }
+}
+
+export default Footer;
